f96a976697fb7c51d9ef16f08c408733ac2cda97
Ashik <ashiquejava@gmail.com>
2018-04-26 03:42:03 +0600

    typeof operator

---
+++ Project2/typeof.js
@@ -0,0 +1,45 @@
+// PRIMITIVE
+
+// STRING
+const name = 'Ashik';
+console.log(typeof name);
+
+//Number
+const age = 45;
+console.log(typeof age);
+
+//Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+
+//Null
+const car = null;
+console.log(typeof car);
+
+//undefined
+let test;
+console.log(typeof test);
+
+//Symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+//Reference
+
+//Array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+const address = {
+  city: 'Dhaka',
+  state: 'MA'
+}
+console.log(typeof address);
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
+
+
+
+
