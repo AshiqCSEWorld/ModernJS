a4d5579a7f3c747e9cf92e99b32c4566349ad372
Ashik <ashiquejava@gmail.com>
2018-04-25 23:55:39 +0600

    Different types of console

---
+++ Project1/app.js
@@ -0,0 +1,29 @@
+//log to console
+console.log("Hello World");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({
+  a: 1,
+  b: 2
+});
+console.table({
+  a: 1,
+  b: 2
+});
+console.error("This is some error");
+console.log('Hello Boss');
+console.warn('This is warning');
+console.log('HIII');
+
+console.time('');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd("");
+
+
+
